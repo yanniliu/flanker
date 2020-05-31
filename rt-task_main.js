88db758a2278bba6ca6331317eeb7b3b61b07ef4
yanniliu <33734801+yanniliu@users.noreply.github.com>
2020-05-31 12:08:53 -0400

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -84,9 +84,9 @@
     var test_procedure = {
       timeline: [fixation, test],
       timeline_variables: test_stimuli,
-      /*repetitions: 3,
-      randomize_order: true*/
-	    sample: {type: 'fixed-repetitions', size: reps_per_trial_type}
+      repetitions: 3,
+      randomize_order: true
+	   /* sample: {type: 'fixed-repetitions', size: reps_per_trial_type} */
     }
         /*defining debriefing block*/
         var debrief = {
